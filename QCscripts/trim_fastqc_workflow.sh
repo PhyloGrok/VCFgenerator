e68dc6751a3364329fdc10beb1c1c0e7472b259d
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-06-13 13:51:21 -0400

    trimmomatic and fastqc on gzipped files

---
+++ QCscripts/trim_fastqc_workflow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#LloydJonesIII
+## Trimmomatic Loop 
+
+for infile in ../../media/volume/sdb/sra/pH/fastq/*_1.fastq.gz 
+do 
+base=$(basename ${infile} _1.fastq.gz) \ 
+trimmomatic PE ../../media/volume/sdb/sra/pH/fastq/${infile} ../../media/volume/sdb/sra/pH/fastq/${base}_2.fastq.gz \
+../../media/volume/sdb/sra/pH/fastq/${base}_1.trim.fastq.gz ../../media/volume/sdb/sra/pH/fastq/${base}_1.untrim.fastq.gz \
+../../media/volume/sdb/sra/pH/fastq/${base}_2.trim.fastq.gz ../../media/volume/sdb/sra/pH/fastq/${base}_2.untrim.fastq.gz \
+SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:../../media/volume/sdb/sra/pH/fastq/NexteraPE-PE.fa:2:40:15
+done
+
+#### Loop ends 
+
+## need to run fastqc on trimmed files only 
+fastqc ../../media/volume/sdb/sra/pH/fastq/*.trim.fastq.gz
