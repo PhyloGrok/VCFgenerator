c9db9838fb876fc2884fd8661328383d0f6317b6
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-06-21 14:09:05 -0400

    Update RetrieveGenomes.sh

--- RetrieveData/RetrieveGenomes.sh
+++ RetrieveData/RetrieveGenomes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## download all genomic fastas from a specific higher taxa into a single multifasta
+ esearch -db genome  -query "txid22" | elink -target nuccore | efetch -format fasta  > txid22.fasta
+
+
 ## use a specific taxon id to retrieve the assembly accession uids:
 esearch -db genome -query "txid70863" | efetch -format docsum | xtract -pattern DocumentSummary -element Assembly_Accession
 
