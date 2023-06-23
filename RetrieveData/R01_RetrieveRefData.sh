0dbe116116072029031fd83cf1f0d6f1fcee95c6
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-06-23 10:58:47 -0400

    Update R01_RetrieveRefData.sh

--- RetrieveData/R01_RetrieveRefData.sh
+++ RetrieveData/R01_RetrieveRefData.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+## This code uses NCBI Datasets to download reference genomes, annotation files, and metadata for reference taxa genomes
+## These dataset collections will be used for within- and between-species comparisons
 datasets download genome taxon 2242 --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename NRC1complete.zip
