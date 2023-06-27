2bbff380fcfd24c9a63318464c6a89b3585cb5d5
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-06-27 11:14:25 -0400

    python trimmer
    two designs of trimmer shells, should allow for multiple instances of trimmomatic to run in parallel 
    trimmer2 == two instances of trimmomatic running in parallel 
    trimmer3 == three instances of trimmomatic running in parallel

---
+++ QCscripts/trimmer3.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+#LloydJonesIII
+trimmomatic PE ../../media/volume/sdb/$1/fastq/${2}_1.fastq.gz ../../media/volume/sdb/$1/fastq/${2}_2.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${2}_1.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${2}_1.untrim.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${2}_2.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${2}_2.untrim.fastq.gz \
+SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:../../media/volume/sdb/NexteraPE-PE.fa:2:40:15 ; \
+trimmomatic PE ../../media/volume/sdb/$1/fastq/${3}_1.fastq.gz ../../media/volume/sdb/$1/fastq/${2}_2.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${3}_1.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${3}_1.untrim.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${3}_2.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${3}_2.untrim.fastq.gz \
+SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:../../media/volume/sdb/NexteraPE-PE.fa:2:40:15 ; \
+trimmomatic PE ../../media/volume/sdb/$1/fastq/${4}_1.fastq.gz ../../media/volume/sdb/$1/fastq/${2}_2.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${4}_1.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${4}_1.untrim.fastq.gz \
+../../media/volume/sdb/$1/fastq/trimmed/${4}_2.trim.fastq.gz ../../media/volume/sdb/$1/fastq/untrimmed/${4}_2.untrim.fastq.gz \
+SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:../../media/volume/sdb/NexteraPE-PE.fa:2:40:15
