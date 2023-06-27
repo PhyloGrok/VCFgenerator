f297f8def7b18368735636371d75539836b2af6e
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-06-27 16:39:55 -0400

    Update trim_fastqc_workflow.sh
    Solving this trimmomatic command issue, the first $infile argument is already defined by its filepath and does not need it specified.

--- QCscripts/trim_fastqc_workflow.sh
+++ QCscripts/trim_fastqc_workflow.sh
@@ -5,7 +5,7 @@
 for infile in ../../media/volume/sdb/sra/pH/fastq/*_1.fastq.gz 
 do 
 base=$(basename ${infile} _1.fastq.gz) \ 
-trimmomatic PE ../../media/volume/sdb/sra/pH/fastq/${infile} ../../media/volume/sdb/sra/pH/fastq/${base}_2.fastq.gz \
+trimmomatic PE ${infile} ../../media/volume/sdb/sra/pH/fastq/${base}_2.fastq.gz \
 ../../media/volume/sdb/sra/pH/fastq/${base}_1.trim.fastq.gz ../../media/volume/sdb/sra/pH/fastq/${base}_1.untrim.fastq.gz \
 ../../media/volume/sdb/sra/pH/fastq/${base}_2.trim.fastq.gz ../../media/volume/sdb/sra/pH/fastq/${base}_2.untrim.fastq.gz \
 SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:../../media/volume/sdb/sra/pH/fastq/NexteraPE-PE.fa:2:40:15
