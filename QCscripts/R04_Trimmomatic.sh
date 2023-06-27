3c9424dc8ac3766c51a38f0da7f7f55931a8b8a8
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-06-27 16:45:55 -0400

    Update R04_Trimmomatic.sh

--- QCscripts/R04_Trimmomatic.sh
+++ QCscripts/R04_Trimmomatic.sh
@@ -7,7 +7,7 @@ read -p "Please name your Project, please use same name as search shell: " pname
 ## Trimmomatic Loop 
 for infile in ../../media/volume/sdb/$pname/fastq/*_1.fastq.gz; 
 do base=$(basename ${infile} _1.fastq.gz);  
-trimmomatic PE ../../media/volume/sdb/$pname/fastq/${infile} ../../media/volume/sdb/$pname/fastq/${base}_2.fastq.gz ../../media/volume/sdb/$pname/fastq/trimmed/${base}_1.trim.fastq.gz ../../media/volume/sdb/$pname/fastq/untrimmed/${base}_1.untrim.fastq.gz ../../media/volume/sdb/$pname/fastq/trimmed/${base}_2.trim.fastq.gz ../../media/volume/sdb/$pname/fastq/untrimmed/${base}_2.untrim.fastq.gz;
+trimmomatic PE ${infile} ../../media/volume/sdb/$pname/fastq/${base}_2.fastq.gz ../../media/volume/sdb/$pname/fastq/trimmed/${base}_1.trim.fastq.gz ../../media/volume/sdb/$pname/fastq/untrimmed/${base}_1.untrim.fastq.gz ../../media/volume/sdb/$pname/fastq/trimmed/${base}_2.trim.fastq.gz ../../media/volume/sdb/$pname/fastq/untrimmed/${base}_2.untrim.fastq.gz;
 SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 done
 #### Loop ends 
