6f853bec650a63b812bcfdcd28218c88ef13dbf6
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-06-27 11:17:47 -0400

    3rd Hub script iteration
    6/27/23 most up to date version of the python hub script

---
+++ Setup/trial3.py
@@ -0,0 +1,12 @@
+## LloydJonesIII
+import subprocess
+x=True
+ylist=['y','yes','Yes','Y']
+while x == True:
+	user1 = input('Please enter the name of your project, no spaces or special characters allowed:    ')
+	user2 = input('Please enter the Project ID you are using for data:    ')
+	user3 = input('Please input your reference genomes taxon id, numbers only no spaces:    ')
+	search = subprocess.call(["bash","./R03.sh",user1,user2,user3]) ## Search Shell script
+	#split  = subprocess.call(["bash","./Split.sh",user1] ## text file splitting Shell script
+	#variant_calling = 
+## testing python variable input into command line scripts 
