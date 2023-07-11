cdf05c26039e1b7998d012cc74156823c65d6042
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-07-11 11:37:47 -0400

    Update RetrieveGenomes.sh

--- RetrieveData/RetrieveGenomes.sh
+++ RetrieveData/RetrieveGenomes.sh
@@ -4,8 +4,13 @@
  esearch -db genome  -query "txid22" | elink -target nuccore | efetch -format fasta  > txid22.fasta
 
 
+
 ## use a specific taxon id to retrieve the assembly accession uids:
 esearch -db genome -query "txid70863" | efetch -format docsum | xtract -pattern DocumentSummary -element Assembly_Accession
+
+
+## Download a specific RefSeq Assembly based on its Assembly ID (GCF)**
+esearch -db assembly -query GCF_000006805.1 | elink -target nucleotide -name assembly_nuccore_refseq | efetch -format fasta > GCF_000006805.1.fa
 
 ## use an assembly accession ID to download a genomic fasta
 esearch -db assembly -query GCA_000146165.2 | elink -target nucleotide -name assembly_nuccore_refseq | efetch -format fasta > GCA_000146165.2.fa
