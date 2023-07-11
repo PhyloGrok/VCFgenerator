f7b795e4a07c19afd756ad80713683b7c9442b90
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-07-11 11:49:07 -0400

    Update R03.sh

--- Python_Hub/R03.sh
+++ Python_Hub/R03.sh
@@ -20,6 +20,8 @@ mkdir ../../media/volume/sdb/$1/assembly/results/bcf
 mkdir ../../media/volume/sdb/$1/assembly/results/vcf
 ## Web-search
 esearch -db sra -query "${2}" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > ../../media/volume/sdb/$1/${1}.txt
+
+## The EDirect retrieval for RefSeq genome is retrieving the Halobacterium 91-R6 (DSM 3754), for the pH experiment, it needs the NRC-1 genome
 datasets download genome taxon $3 --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename ../../media/volume/sdb/$1/assembly/${3}.zip
 prefetch --option-file ../../media/volume/sdb/$1/${1}.txt -O ../../media/volume/sdb/$1/sra/
 fasterq-dump --outdir ../../media/volume/sdb/$1/fastq ../../media/volume/sdb/$1/sra/SRR*
