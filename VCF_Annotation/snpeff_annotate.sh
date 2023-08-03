fa55f83ca56d14ad4955b1f6c77a2792193c8c6d
Nhi Luu <98994882+nluu1@users.noreply.github.com>
2023-08-02 21:34:47 -0400

    Update snpeff_annotate.sh

--- VCF_Annotation/snpeff_annotate.sh
+++ VCF_Annotation/snpeff_annotate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Parse command-line options
-while getopts ":d:i:o:" opt; do
+while getopts "c:d:i:o:" opt; do
 case $opt in
+c) config_file=$OPTARG
+;;
 d) database=$OPTARG
 ;;
 i) vcf_dir=$OPTARG
@@ -18,7 +20,11 @@ echo "Option -$OPTARG requires an argument." >&2
 esac
 
 done
-
+if [ -z "$config_file" ]; then
+echo "Error: Option -c requires an argument."
+exit 1
+ fi
+ 
 if [ -z "$database" ]; then
 echo "Error: Option -d requires an argument."
 exit 1
@@ -41,6 +47,6 @@ for vcf_file in "$vcf_dir"*_final_variants.vcf; do
  # Extract file name without extension
 file_name=$(basename "$vcf_file" _final_variants.vcf)
  # Run snpEff annotation command
-snpEff ann -no-downstream -no-upstream -c resources/SnpEff/snpEff.config -s $output_dir${file_name}_summary.html -csvStats $output_dir${file_name}_annotated.csv $database $vcf_file > $output_dir${file_name}_final_variants_annotated.vcf
- echo "snpEff ann -no-downstream -no-upstream -c resources/SnpEff/snpEff.config $database $vcf_file > $output_dir${file_name}_final_variants_annotated.vcf -s $output_dir${file_name}_summary.html -csvStats $output_dir${file_name}_annotated.csv"
+snpEff ann -no-downstream -no-upstream -c $config_file -s $output_dir${file_name}_summary.html -csvStats $output_dir${file_name}_annotated.csv $database $vcf_file > $output_dir${file_name}_final_variants_annotated.vcf
+ echo "snpEff ann -no-downstream -no-upstream -c $config_file $database $vcf_file > $output_dir${file_name}_final_variants_annotated.vcf -s $output_dir${file_name}_summary.html -csvStats $output_dir${file_name}_annotated.csv"
 done
