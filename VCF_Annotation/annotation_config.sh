f0ba4dcddf8d5b5c00811f70e0ccbd5af3fbe0ab
Nhi Luu <98994882+nluu1@users.noreply.github.com>
2023-08-04 12:06:31 -0400

    Update annotation_config.sh

--- VCF_Annotation/annotation_config.sh
+++ VCF_Annotation/annotation_config.sh
@@ -15,26 +15,23 @@ FIRSTDIR=$(dirname $(dirname $(which snpEff)))
 TMP=$(dirname $(readlink -s `which snpEff`))
 SNPEFFDIR=${FIRSTDIR}${TMP/../}
 
-sudo mkdir -p $RESOURCE_PATH/SnpEff
+mkdir -p $RESOURCE_PATH/SnpEff
 
 SNPEFF_PATH="/media/volume/sdb/$1/assembly/resources/SnpEff"
-sudo cp $SNPEFFDIR/snpEff.config  $SNPEFF_PATH
-sudo chmod a+rw $SNPEFF_PATH/snpEff.config
+cp $SNPEFFDIR/snpEff.config  $SNPEFF_PATH
+
 
 echo "                    
 # $ENTRY_INFO, $REFNAME
 $REFNAME.genome : $REFNAME" >> $SNPEFF_PATH/snpEff.config
 
-sudo mkdir -p $SNPEFF_PATH/data/$REFNAME
+mkdir -p $SNPEFF_PATH/data/$REFNAME
 
-sudo cp $REFPATH/GCF_*_genomic.fna $SNPEFF_PATH/data/$REFNAME/sequences.fa
-sudo cp $REFPATH/genomic.gtf $SNPEFF_PATH/data/$REFNAME/genes.gtf
-sudo cp $REFPATH/protein.faa $SNPEFF_PATH/data/$REFNAME/protein.fa
-sudo cp $REFPATH/genomic.gff $SNPEFF_PATH/data/$REFNAME/genes.gff
-sudo cp $REFPATH/cds_from_genomic.fna $SNPEFF_PATH/data/$REFNAME/cds.fa
-
-sudo chmod a+rw $SNPEFF_PATH/data/$REFNAME/*
-sudo chmod a+w $SNPEFF_PATH/data/$REFNAME/
+cp $REFPATH/GCF_*_genomic.fna $SNPEFF_PATH/data/$REFNAME/sequences.fa
+cp $REFPATH/genomic.gtf $SNPEFF_PATH/data/$REFNAME/genes.gtf
+cp $REFPATH/protein.faa $SNPEFF_PATH/data/$REFNAME/protein.fa
+cp $REFPATH/genomic.gff $SNPEFF_PATH/data/$REFNAME/genes.gff
+cp $REFPATH/cds_from_genomic.fna $SNPEFF_PATH/data/$REFNAME/cds.fa
 
 snpEff build -Xmx4g -noCheckCds -noCheckProtein -gff3 -c $SNPEFF_PATH/snpEff.config -v $REFNAME
 
