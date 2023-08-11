0fe90d3e00fae21c7be2c3d01fdc417714838c44
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-08-11 11:27:49 -0400

    replacing python hub directory
    * deleting old files and scripts to most up to date files and scripts in a model to how the user will run the repository

--- Python_Hub/fastqc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#LloydJonesIII
-echo 'Running the fastqc'
-## fastqc process 
-fastqc ../../media/volume/sdb/$1/fastq/trimmed/*.trim.fastq.gz
-echo 'fastqc has finished'
--- Python_Hub/variant6.py
+++
@@ -1,9 +0,0 @@
-## LloydJonesIII
-import subprocess 
-import numpy as np
-# Import Variables from the Controller py script
-with open('inputs.txt', 'r') as userfile:
-    all = userfile.read().splitlines()
-userfile.close()
-# run the variant calling process 
-subprocess.call(["bash","./VCF.sh", all[0]])
