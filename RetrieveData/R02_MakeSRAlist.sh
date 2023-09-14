b336b35d07ec04e1cbe1d3ed4c2b3e830a4852f1
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-09-14 15:34:58 -0400

    Delete RetrieveData directory

--- RetrieveData/R02_MakeSRAlist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## Requires EDirect installed
-## Download a list of all SRA runs associated with a specific bioproject
-esearch -db sra -query "PRJNA541441" | efetch -format docsum | xtract -pattern Runs -ACC @acc -element "&ACC" > pH.txt
