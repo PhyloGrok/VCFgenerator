b5a32e369a4f122b50e4d05d3ec07346e57cf193
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-09-14 15:34:06 -0400

    Delete QCscripts directory

--- QCscripts/trim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for infile in *_1.fastq.gz
-do
-base=$(basename ${infile} _1.fastq.gz)
-trimmomatic PE ${infile} ${base}_2.fastq.gz \
-${base}_1.trim.fastq.gz ${base}_1.untrim.fastq.gz \
-${base}_2.trim.fastq.gz ${base}_2.untrim.fastq.gz \
-SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
-done
