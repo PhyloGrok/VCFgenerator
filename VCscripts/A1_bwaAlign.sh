3a9342680832589f77662097c3f0b17f8d184e64
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-09-14 15:34:41 -0400

    Delete VCscripts directory

--- VCscripts/A1_bwaAlign.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for infile in ../sra/pH/fastq/*_1.trim.fastq.gz
-do
-        base=$(basename ${infile} _1.trim.fastq.gz)
-        echo ${infile}
-        echo $base
-        bwa mem HsRefGenome.fa ../sra/pH/fastq/${base}_1.trim.fastq.gz /sra/pH/fastq/${base}_2.trim.fastq.gz > results/sam/${base}.aligned.sam
-done
--- VCscripts/A2_SamToBam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for infile in results/sam/*.aligned.sam
-
-do
-        base=$(basename ${infile} .aligned.sam)
-        echo ${infile}
-        echo $base
-        samtools view -S -b results/sam/${base}.aligned.sam > results/bam/${base}.aligned.bam
-done
