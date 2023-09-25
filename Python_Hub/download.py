d7288c8fa9d43df9271896cadbec54eb7797f68a
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-09-25 14:23:17 -0400

    Update download.py

--- Python_Hub/download.py
+++ Python_Hub/download.py
@@ -11,12 +11,12 @@ import numpy as np
 with open(f'inputs.txt','r') as userfile:
     inputs = userfile.read().splitlines()
 userfile.close()
+## Run the first layer of searches and downloads 
+search = subprocess.call(["bash","./import.sh",inputs[0],inputs[1],inputs[2],inputs[4]])
 # Import sra text file and converts it to a python list 
 with open(f'{inputs[4]}/{inputs[0]}/{inputs[0]}.txt','r') as userfile:
     srafile = userfile.read().splitlines()
 userfile.close()
-## Run the first layer of searches and downloads 
-search = subprocess.call(["bash","./import.sh",inputs[0],inputs[1],inputs[2],inputs[4]])
 ## Splitter check 
 yeslist = ['y','Y','yes']
 if inputs[3] in yeslist:
