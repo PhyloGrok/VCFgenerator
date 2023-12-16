7452e96916e126a706b11f8cce134093d7df66d2
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2023-12-16 10:31:08 -0500

    Update zip.sh

--- Python_Hub/zip.sh
+++ Python_Hub/zip.sh
@@ -4,5 +4,6 @@
 ## 1 = directory name
 ## 2 = file path
 echo $1 ' Batch Gzip process has begun'
-gzip $2/$1/fastq/untrimmed/*.fastq
+gzip $2/$1/fastq/untrimmed/*.fastq  ## This runs fine from the command line as "gzip /media/volume/sdb/test1/fastq/untrimmed/*.fastq"
+## The error from the script run is "file/direcotry not found", check for the wrong import variables
 echo $1 ' Batch have been Gzipped'
