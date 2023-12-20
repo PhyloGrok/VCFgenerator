9cd38085efdbc1ac09d34ac1635614ab32dc147c
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-12-20 11:16:53 -0500

    Update splitter.sh

--- Python_Hub/splitter.sh
+++ Python_Hub/splitter.sh
@@ -2,11 +2,17 @@
 ##LJones, Jrobinson, NLuu 
 ## Code Inspiration
 # biostars.org/p/141256/
-# User Inputs
-## 1 = project name 
-## 2 = file path
-## 3 = file name 
+## User Inputs 
+## 1 = directory name
+## 2 = project ID
+## 3 = taxon ID
+## 4 = file path
+## 5 = SRR file name 
+echo $1
+echo $2
+echo $3
+echo $4
 # Paired end fastq splitter 
-paste - - - - - - - - < $2/$1/fastq/${3}.fastq \ | tee >(cut -f 1-4 | tr "\t" "\n" > $2/$1/fastq/${3}_1.fastq) \ | cut -f 5-8 | tr "\t" "\n" > $2/$1/fastq/${3}_2.fastq)
+paste - - - - - - - - < $4/$1/fastq/${5}.fastq \ | tee >(cut -f 1-4 | tr "\t" "\n" > $4/$1/fastq/${5}_1.fastq) \ | cut -f 5-8 | tr "\t" "\n" > $4/$1/fastq/${5}_2.fastq)
 # delete interleaved file after splitting 
-rm $2/$1/fastq/${3}.fastq 
+rm $4/$1/fastq/${5}.fastq 
