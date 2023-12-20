3c3fd888896452048ce56f98e82c959068ecb73a
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-12-20 11:18:42 -0500

    Update thetrimmer.sh
    doubled threads standardized input variables

--- Python_Hub/thetrimmer.sh
+++ Python_Hub/thetrimmer.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 #LloydJonesIII
 # User Inputs 
-## 1 = project name
-## 2 = file name
-## 3 = file path
-trimmomatic PE -threads 4 $3/$1/fastq/${2}_1.fastq.gz $3/$1/fastq/${2}_2.fastq.gz \
-$3/$1/fastq/trimmed/${2}_1.trim.fastq.gz $3/$1/fastq/untrimmed/${2}_1.untrim.fastq.gz \
-$3/$1/fastq/trimmed/${2}_2.trim.fastq.gz $3/$1/fastq/untrimmed/${2}_2.untrim.fastq.gz \
+## 1 = directory name
+## 2 = project ID
+## 3 = taxon ID
+## 4 = file path
+## 5 = file name
+trimmomatic PE -threads 8 $4/$1/fastq/untrimmed/${5}_1.fastq.gz $4/$1/fastq/untrimmed/${5}_2.fastq.gz \
+$4/$1/fastq/trimmed/${5}_1.trim.fastq.gz $4/$1/fastq/untrimmed/${5}_1.untrim.fastq.gz \
+$4/$1/fastq/trimmed/${5}_2.trim.fastq.gz $4/$1/fastq/untrimmed/${5}_2.untrim.fastq.gz \
 SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
