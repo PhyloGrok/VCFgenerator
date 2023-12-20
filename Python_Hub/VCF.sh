b1a44878cf9993f3f0c1e0cba0163838215414ca
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2023-12-20 11:07:15 -0500

    Update VCF.sh

--- Python_Hub/VCF.sh
+++ Python_Hub/VCF.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 #LJones, JRobinson, NLuu
 ## User Inputs 
-# 1 = directory name
-# 2 = directoy path
+## 1 = directory name
+## 2 = project ID
+## 3 = taxon ID
+## 4 = file path
+echo $1
+echo $2
+echo $3
+echo $4
 ## Set the index
-bwa index $2/$1/assembly/reference/ref_genome.fasta
+bwa index $4/$1/assembly/reference/ref_genome.fasta
 ## Variant Calling Loop
-for x in $2/$1/fastq/*_1.fastq.gz
+for x in $4/$1/fastq/untrimmed/*_1.fastq.gz
 do
 name=$(basename ${x} _1.fastq.gz)
 ## Generate the SAM files
-bwa mem -t 4 $2/$1/assembly/reference/ref_genome.fasta $2/$1/fastq/trimmed/${name}_1.trim.fastq.gz $2/$1/fastq/trimmed/${name}_2.trim.fastq.gz > $2/$1/assembly/results/sam/${name}.aligned.sam
+bwa mem -t 4 $4/$1/assembly/reference/ref_genome.fasta $4/$1/fastq/trimmed/${name}_1.trim.fastq.gz $4/$1/fastq/trimmed/${name}_2.trim.fastq.gz > $4/$1/assembly/results/sam/${name}.aligned.sam
 done
 ## Generate the BAM files
-for x in $2/$1/fastq/*_1.fastq.gz
+for x in $4/$1/fastq/untrimmed/*_1.fastq.gz
 do
 name=$(basename ${x} _1.fastq.gz)
-samtools view -S -b $2/$1/assembly/results/sam/${name}.aligned.sam > $2/$1/assembly/results/bam/${name}.aligned.bam
-samtools sort -o $2/$1/assembly/results/bam/${name}.aligned.sorted.bam $2/$1/assembly/results/bam/${name}.aligned.bam
-samtools index $2/$1/assembly/results/bam/${name}.aligned.sorted.bam
+samtools view -S -b $4/$1/assembly/results/sam/${name}.aligned.sam > $4/$1/assembly/results/bam/${name}.aligned.bam
+samtools sort -o $4/$1/assembly/results/bam/${name}.aligned.sorted.bam $4/$1/assembly/results/bam/${name}.aligned.bam
+samtools index $4/$1/assembly/results/bam/${name}.aligned.sorted.bam
 done
-rm -r ${2}/${1}/assembly/results/sam/
+#rm -r ${4}/${1}/assembly/results/sam/
 ## Read coverage of positions in the genome
-for x in $2/$1/fastq/*_1.fastq.gz
+for x in $4/$1/fastq/untrimmed/*_1.fastq.gz
 do
 name=$(basename ${x} _1.fastq.gz)
-bcftools mpileup -O b -o $2/$1/assembly/results/bcf/${name}_raw.bcf -f $2/$1/assembly/reference/ref_genome.fasta $2/$1/assembly/results/bam/${name}.aligned.sorted.bam
+bcftools mpileup -O b -o $4/$1/assembly/results/bcf/${name}_raw.bcf -f $4/$1/assembly/reference/ref_genome.fasta $4/$1/assembly/results/bam/${name}.aligned.sorted.bam
 done
-rm -r ${2}/${1}/assembly/results/bam/
+#rm -r ${4}/${1}/assembly/results/bam/
 ## SNV Detection
-for x in $2/$1/fastq/*_1.fastq.gz
+for x in $4/$1/fastq/untrimmed/*_1.fastq.gz
 do
 name=$(basename ${x} _1.fastq.gz)
-bcftools call --ploidy 1 -m -v -o $2/$1/assembly/results/vcf/${name}.vcf $2/$1/assembly/results/bcf/${name}_raw.bcf
+bcftools call --ploidy 1 -m -v -o $4/$1/assembly/results/vcf/${name}.vcf $4/$1/assembly/results/bcf/${name}_raw.bcf
 done
-rm -r ${2}/${1}/assembly/results/bcf/
+#rm -r ${4}/${1}/assembly/results/bcf/
 ## Final Variant Calling
-for x in $2/$1/fastq/*_1.fastq.gz
+for x in $4/$1/fastq/untrimmed/*_1.fastq.gz
 do
 name=$(basename ${x} _1.fastq.gz)
-vcfutils.pl varFilter $2/$1/assembly/results/vcf/${name}.vcf > $2/$1/assembly/results/final_vcf/${name}_final_variants.vcf
+vcfutils.pl varFilter $4/$1/assembly/results/vcf/${name}.vcf > $4/$1/assembly/results/final_vcf/${name}_final_variants.vcf
 done
