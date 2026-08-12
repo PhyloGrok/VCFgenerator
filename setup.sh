5ceced3737d96d45844de8c9706323a5d605b7c7
Jeffrey Robinson <31963580+PhyloGrok@users.noreply.github.com>
2026-08-12 16:19:28 +0000

    feat: add interactive container launcher and modified setup.sh with the updated storage volume filepath variable

--- setup.sh
+++ setup.sh
@@ -6,27 +6,44 @@ read -p "Enter your TaxID: " txid
 echo $txid
 #echo $filepath
 
-mkdir -m777 ~/VCF.projects
-mkdir -m777 ~/VCF.projects/$txid/
-mkdir -m777 ~/VCF.projects/$txid/data
-mkdir -m777 ~/VCF.projects/$txid/data/ref_genome
-mkdir -m777 ~/VCF.projects/$txid/data/untrimmed_fastq
-mkdir -m777 ~/VCF.projects/$txid/data/trimmed_fastq
-mkdir -m777 ~/VCF.projects/$txid/results
-mkdir -m777 ~/VCF.projects/$txid/results/sam
-mkdir -m777 ~/VCF.projects/$txid/results/bam
-mkdir -m777 ~/VCF.projects/$txid/results/bcf
-mkdir -m777 ~/VCF.projects/$txid/results/vcf
+# Read DATA_DIR from environment, default to /app/data if empty
+BASE_DATA_DIR="${DATA_DIR}:-/app/data}"
+
+
+# Build the directory tree paths
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/data"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/data/untrimmed_fastq"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/data/trimmed_fastq"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/results"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/results/sam"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/results/bam"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/results/bcf"
+mkdir -m777 "${BASE_DATA_DIR}/VCF.projects/$txid/results/vcf"
+
+
+#mkdir -m777 ~/VCF.projects
+#mkdir -m777 ~/VCF.projects/$txid/
+#mkdir -m777 ~/VCF.projects/$txid/data
+#mkdir -m777 ~/VCF.projects/$txid/data/ref_genome
+#mkdir -m777 ~/VCF.projects/$txid/data/untrimmed_fastq
+#mkdir -m777 ~/VCF.projects/$txid/data/trimmed_fastq
+#mkdir -m777 ~/VCF.projects/$txid/results
+#mkdir -m777 ~/VCF.projects/$txid/results/sam
+#mkdir -m777 ~/VCF.projects/$txid/results/bam
+#mkdir -m777 ~/VCF.projects/$txid/results/bcf
+#mkdir -m777 ~/VCF.projects/$txid/results/vcf
 
 
 #datasets download genome taxon $txid --reference --include genome --filename ~/VCF.projects/$txid/data/ref_genome/$txid.zip
-datasets download genome taxon $txid --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename ~/VCF.projects/$txid/data/ref_genome/$txid.zip
+datasets download genome taxon $txid --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename ${BASE_DATA_DIR}/VCF.projects/$txid/data/$txid.zip
 
 
-unzip ~/VCF.projects/$txid/data/ref_genome/$txid.zip -d ~/VCF.projects/$txid/data/ref_genome
+unzip ${BASE_DATA_DIR}/VCF.projects/$txid/data/$txid.zip -d ~/VCF.projects/$txid/data/
 #cd /media/volume/sdc/S25/data/untrimmed_fastq
 
 esearch -db sra -query ""txid$txid"[Orgn] AND "genomic"[Source] AND "paired"[Layout] AND "illumina"[Platform] AND "wgs"[Strategy] AND filetype "fastq"[Filter] AND strategy "genome"[Filter]" | efetch -format docsum | xtract -pattern Runs -ACC @acc -element "&ACC" > ~/VCF.projects/$txid/data/SraList_$txid.txt
 
-prefetch --option-file ~/VCF.projects/$txid/data/SraList_$txid.txt -O ~/VCF.projects/$txid/data/sra/
-fasterq-dump ~/VCF.projects/$txid/data/sra/SRR* -O ~/VCF.projects/$txid/data/untrimmed_fastq/
+prefetch --option-file ${BASE_DATA_DIR}/VCF.projects/$txid/data/SraList_$txid.txt -O ${BASE_DATA_DIR}/VCF.projects/$txid/data/sra/
+fasterq-dump ${BASE_DATA_DIR}/VCF.projects/$txid/data/sra/SRR* -O ${BASE_DATA_DIR}/VCF.projects/$txid/data/untrimmed_fastq/
