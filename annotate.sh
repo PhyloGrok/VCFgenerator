9859800c902ccc99dddf9cff5522118d1a554e8b
Jeffrey Robinson <31963580+PhyloGrok@users.noreply.github.com>
2026-08-13 11:48:38 +0000

    chore: delete old annotate file

--- annotate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-datasets download genome taxon $3 --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename $4/$1/assembly/${3}.zip
-
-unzip ~/VCF.projects/$txid/data/ref_genome/$txid.zip -d ~/VCF.projects/$txid/data/ref_genome
